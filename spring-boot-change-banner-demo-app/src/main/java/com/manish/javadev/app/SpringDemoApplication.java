de6af4d0981b1543a3e772d86a388fb2f9188cd6
Manish Srivastava <msmanish95@gmail.com>
2017-09-27 16:41:20 +0530

    Code Changes

--- spring-boot-change-banner-demo-app/src/main/java/com/manish/javadev/app/SpringDemoApplication.java
+++ spring-boot-change-banner-demo-app/src/main/java/com/manish/javadev/app/SpringDemoApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirstSpringBootDemoAppApplication {
+public class SpringDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(FirstSpringBootDemoAppApplication.class);
+		SpringApplication app = new SpringApplication(SpringDemoApplication.class);
 		app.setBannerMode(Banner.Mode.CONSOLE);
 		app.run(args);
 	}
--- spring-boot-change-banner-demo-app/src/test/java/com/manish/javadev/app/SpringDemoApplicationTests.java
+++ spring-boot-change-banner-demo-app/src/test/java/com/manish/javadev/app/SpringDemoApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FirstSpringBootDemoAppApplicationTests {
+public class SpringDemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
